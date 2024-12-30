b88e0807b5a488d2521bea59fe265d87933116a2
VeljkoBogdan <134805608+VeljkoBogdan@users.noreply.github.com>
2024-12-31 00:25:30 +0100

    Fixed misspelling and pretty-fied the text in comments

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "raylib.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"	// Utility header for SearchAndSetResourceDir
 
 int main ()
 {
@@ -25,30 +25,30 @@ int main ()
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
-	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	// Game loop
+	while (!WindowShouldClose())		// Run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
-		// drawing
+		// Drawing
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
+		// Draw some text using the default font
 		DrawText("Hello Raylib", 200,200,20,WHITE);
 
-		// draw our texture to the screen
+		// Draw our texture to the screen
 		DrawTexture(wabbit, 400, 200, WHITE);
 		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		// Dnd the frame and get ready for the next one (display frame, poll input, etc...)
 		EndDrawing();
 	}
 
-	// cleanup
-	// unload our texture so it can be cleaned up
+	// Cleanup
+	// Unload our texture so it can be cleaned up
 	UnloadTexture(wabbit);
 
-	// destroy the window and cleanup the OpenGL context
+	// Destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
