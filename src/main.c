0708b26f239592dcbd0208890e447daf0ed6c4e8
miracade <devinc4@uci.edu>
2025-08-04 22:39:29 -0700

    hampter spin

--- src/main.c
+++ src/main.c
@@ -8,45 +8,90 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include "raylib.h"
+#include "raymath.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h" // utility header for SearchAndSetResourceDir
 
-int main ()
+#include "math.h"
+
+const int render_w = 320;
+const int render_h = 240;
+const int window_scale = 2;
+const int window_w = render_w * window_scale;
+const int window_h = render_h * window_scale;
+
+int main()
 {
+	SetTraceLogLevel(LOG_WARNING);
+
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	// SetTargetFPS(120);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(window_w, window_h, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+	Image baby = LoadImage("baby.png");
+	Image baby_flipped = ImageCopy(baby);
+	ImageFlipHorizontal(&baby_flipped);
+
+	Image render = GenImageColor(render_w, render_h, BLACK);
+	ImageFormat(&render, PIXELFORMAT_UNCOMPRESSED_R5G6B5);
+
 	// game loop
-	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
+	int ticks = 0;
+	float spins = 0;
+	while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+		spins += GetFrameTime();
 		// drawing
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		// reset image
+		ImageClearBackground(&render, SKYBLUE);
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
+		ImageDrawText(&render, "Hello Raylib", 0, 0, 20, WHITE);
+
+		// draw onto image
+		float tlx = Remap(-fabsf(sinf(spins * PI)), -1, 1, 0, baby.width);
+		float w = Remap(fabsf(sinf(spins * PI)), -1, 1, -baby.width, baby.width);
+		if (w >= 5.0f)
+		{
+			ImageDraw(&render,
+								(sinf(spins * PI) > 0 ? baby : baby_flipped),
+								(Rectangle){0, 0, baby.width, baby.height},
+								(Rectangle){
+										tlx,
+										0,
+										w,
+										baby.height},
+								WHITE);
+		}
+
+		Texture2D tex = LoadTextureFromImage(render);
+		SetTextureFilter(tex, TEXTURE_FILTER_POINT);
+		DrawTextureEx(tex, (Vector2){0, 0}, 0, window_scale, WHITE);
+		DrawFPS(0, 0);
+
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
+		UnloadTexture(tex);
+
+		++ticks;
 	}
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	UnloadImage(render);
+	UnloadImage(baby);
+	UnloadImage(baby_flipped);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
