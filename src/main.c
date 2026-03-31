8073c2af18a0988c3eb9bebaea90b9d837d6cf50
Jeffery Myers <JeffM2501@Gmail.com>
2026-03-31 10:01:22 -0700

    default to a size that works on 720p monitors

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(800, 600, "Hello Raylib");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
