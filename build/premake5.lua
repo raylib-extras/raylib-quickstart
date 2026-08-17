4f2f30c84cc5c5e6991d3fcb364d96392d3fd378
JeffMyers <JeffM2501@gmail.com>
2026-08-17 08:48:20 -0700

    get rid of the include dir and just use the src dir

--- build/premake5.lua
+++ build/premake5.lua
@@ -190,13 +190,13 @@ if (downloadRaylib) then
 
         vpaths 
         {
-            ["Header Files/*"] = { "../include/**.h",  "../include/**.hpp", "../src/**.h", "../src/**.hpp"},
+            ["Header Files/*"] = { "../src/**.h",  "../src/**.hpp"},
             ["Source Files/*"] = {"../src/**.c", "src/**.cpp"},
             ["Windows Resource Files/*"] = {"../src/**.rc", "../src/**.ico"},
             ["Game Resource Files/*"] = {"../resources/**"},
         }
         
-        files {"../src/**.c", "../src/**.cpp", "../src/**.h", "../src/**.hpp", "../include/**.h", "../include/**.hpp"}
+        files {"../src/**.c", "../src/**.cpp", "../src/**.h", "../src/**.hpp"}
         
         filter {"system:windows", "action:vs*"}
             files {"../src/*.rc", "../src/*.ico"}
